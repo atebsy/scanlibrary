d535d4a8f884dcc1d5911cc39bd2b3830e589bef
Joel Ateba Awono <atebajoel@gmail.com>
2023-08-23 01:52:30 +0100

    Update ScanActivity.java
    skip pickImage fragment

--- library/src/main/java/com/scanlibrary/ScanActivity.java
+++ library/src/main/java/com/scanlibrary/ScanActivity.java
@@ -9,6 +9,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
+
 /**
  * Created by jhansi on 28/03/15.
  */
@@ -22,15 +24,32 @@ public class ScanActivity extends Activity implements IScanner, ComponentCallbac
     }
 
     private void init() {
-        PickImageFragment fragment = new PickImageFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(ScanConstants.OPEN_INTENT_PREFERENCE, getPreferenceContent());
-        bundle.putInt("quality", getIntent().getIntExtra("quality", 1));
-        fragment.setArguments(bundle);
-        android.app.FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.content, fragment);
-        fragmentTransaction.commit();
+        String file = getApplicationContext().getExternalCacheDir().getPath() + getIntent().getIntExtra("file_name", "")
+        Uri uri = Uri.parse("String file location");
+        postImagePick(getBitmap(uri));
+    }
+
+    private Bitmap getBitmap(Uri selectedimg) throws IOException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        try {
+            int quality = getArguments().getInt("quality", 1);
+            options.inSampleSize = quality;
+        } catch (Exception e) {
+            options.inSampleSize = 1;
+        }
+        AssetFileDescriptor fileDescriptor = null;
+        fileDescriptor =
+                getActivity().getContentResolver().openAssetFileDescriptor(selectedimg, "r");
+        Bitmap original
+                = BitmapFactory.decodeFileDescriptor(
+                fileDescriptor.getFileDescriptor(), null, options);
+        return original;
+    }
+
+    protected void postImagePick(Bitmap bitmap) {
+        Uri uri = Utils.getUri(getActivity(), bitmap);
+        bitmap.recycle();
+        onBitmapSelect(uri);
     }
 
     protected int getPreferenceContent() {
@@ -41,6 +60,8 @@ public class ScanActivity extends Activity implements IScanner, ComponentCallbac
     public void onBitmapSelect(Uri uri) {
         ScanFragment fragment = new ScanFragment();
         Bundle bundle = new Bundle();
+        bundle.putInt(ScanConstants.OPEN_INTENT_PREFERENCE, getPreferenceContent());
+        bundle.putInt("quality", getIntent().getIntExtra("quality", 1));
         bundle.putParcelable(ScanConstants.SELECTED_BITMAP, uri);
         fragment.setArguments(bundle);
         android.app.FragmentManager fragmentManager = getFragmentManager();
@@ -63,55 +84,6 @@ public class ScanActivity extends Activity implements IScanner, ComponentCallbac
         fragmentTransaction.commit();
     }
 
-    @Override
-    public void onTrimMemory(int level) {
-        switch (level) {
-            case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
-                /*
-                   Release any UI objects that currently hold memory.
-
-                   The user interface has moved to the background.
-                */
-                break;
-            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
-            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
-            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
-                /*
-                   Release any memory that your app doesn't need to run.
-
-                   The device is running low on memory while the app is running.
-                   The event raised indicates the severity of the memory-related event.
-                   If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
-                   begin killing background processes.
-                */
-                break;
-            case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
-            case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
-            case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
-                /*
-                   Release as much memory as the process can.
-
-                   The app is on the LRU list and the system is running low on memory.
-                   The event raised indicates where the app sits within the LRU list.
-                   If the event is TRIM_MEMORY_COMPLETE, the process will be one of
-                   the first to be terminated.
-                */
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.low_memory)
-                        .setMessage(R.string.low_memory_message)
-                        .create()
-                        .show();
-                break;
-            default:
-                /*
-                  Release any non-critical data structures.
-
-                  The app received an unrecognized memory level value
-                  from the system. Treat this as a generic low-memory message.
-                */
-                break;
-        }
-    }
 
     public native Bitmap getScannedBitmap(Bitmap bitmap, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
 
